e309ce65f12d6d51640ac9dc43221cf437c7287c
Vini <Vini@gmail.com>
2026-04-23 20:12:13 +0530

    Add dev script to run server with auto-restart

---
+++ app.js
@@ -0,0 +1,28 @@
+const http = require('http');
+const server = http.createServer((req, res) => {
+    // res.write('Welcome to our home page')
+    // res.end()
+    if(req.url === '/'){
+        return res.end('Welcome to our home page')
+    }
+    if(req.url === '/about'){
+        return res.end('Here is our short history')
+    }
+    res.end(`
+            <h1>Oops!</h1>
+            <p>We can't seem to find the page you are looking for.</p>
+            <a href="/">Back Home</a>`
+    )
+})
+
+server.listen(5000)
+
+// -------------------
+const _ = require('lodash')
+const items = [1, [2,[3, [4]]]]
+const newItems = _.flattenDeep(items)
+// _.flattenDeep() = convert nested array → single flat array
+console.log(newItems);
+console.log(`Hello People Again`);
+
+
